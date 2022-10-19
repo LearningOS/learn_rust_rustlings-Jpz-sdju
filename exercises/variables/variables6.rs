219ec770a1422b0478059a37317b94b9a6eb3fc9
menthol <59315414+Jpz-sdju@users.noreply.github.com>
2022-10-19 18:49:46 +0000

    update

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,9 @@
 // variables6.rs
 // Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
 
-// I AM NOT DONE
+// 
 
-const NUMBER = 3;
+const NUMBER:u32 = 3u32;
 fn main() {
     println!("Number {}", NUMBER);
 }
